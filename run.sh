0d22a52d4211280862c58b95ffaf5e6e2aa0e7c1
Yijun Yu <y.yu@open.ac.uk>
2019-04-30 21:07:56 +0000

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-live_test --model_path=model/github_java_10.ggnn.aggregation=1.distributed_function=0.hidden_layer_size=200.num_hidden_layer=1.node_dim=200 --pretrained_embeddings_url=embedding/fast_pretrained_vectors.pkl.gz 1.java
+live_test --model_path=model/github_java_10.ggnn.aggregation=1.distributed_function=0.hidden_layer_size=200.num_hidden_layer=1.node_dim=200 --pretrained_embeddings_url=embedding/fast_pretrained_vectors.pkl.gz $1
