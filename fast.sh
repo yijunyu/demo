912db54614a160268e398fe6625b6d66695d9e89
Yijun Yu <y.yu@open.ac.uk>
2019-05-03 19:25:39 +0100

    simplify

--- fast.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$1" == "bash" ]; then
-	docker run --rm -v $(pwd):/e --entrypoint bash -it yijun/fast
-else
-	docker run --rm -v $(pwd):/e --entrypoint bash -it yijun/fast -c "tar cvfz fast.tar.gz -C / -T fast.txt"
-	tar xvfz fast.tar.gz
-	git add usr && git commit -am "update" && git push
-fi
