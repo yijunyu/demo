a034ffd6974b960f72fffff3feb727f5bfa689d7
Yijun Yu <y.yu@open.ac.uk>
2019-05-27 16:24:28 +0000

    update

--- datasets/github_java_10/4/3.java
+++ datasets/github_java_10/4/3.java
@@ -1,27 +1,34 @@
-public static void insertionSort()
-    
+class InsertionSort
+{
+  void sort(int arr[])
+  {
+    int n=arr.length;
+    for(int i=1;i<n;++i)
     {
-    int i = 1;
-
-    while 
-        
-        (i < arr.length)
-    {
-        int j = i;
-        while
-        
-        (j > 0)
-        {
-        if (arr[j].key < arr[j-1].key)
-        {
-            InsertionSortNode tmp = arr[j];
-            arr[j] = arr[j-1];
-            arr[j-1] = tmp;
-        }
-        j = j - 1;
-        }
-        i = i + 1;
+      int key=arr[i];
+      int j=i-1;
+      while(j>=0&& arr[j]>key)
+      {
+        arr[j+1]=arr[j];
+        j=j-1;
+      }
+      arr[j+1]=key;
     }
-    }
+  }
 
-    
+  static void printArray(int arr[])
+  {
+    int n=arr.length;
+    for(int i=0;i<n;++i)
+      System.out.print(arr[i]+" ");
+    System.out.println();
+  }
+
+  public static void main(String args[])
+  {
+    int arr[]={12,11,14,5,6};
+    InsertionSort insertSort=new InsertionSort();
+    insertSort.sort(arr);
+    printArray(arr);
+  }
+}
